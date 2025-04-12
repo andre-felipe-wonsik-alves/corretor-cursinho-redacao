663777bfc88a610cfb452cbf367c5b943e32da1f
andre-felipe-wonsik-alves <aalves.2020@alunos.utfpr.edu.br>
2025-04-12 09:38:53 -0300

    install shad

---
+++ redacao-frontend/src/components/button.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export const Button = ({ children, className, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) => (
+  <button
+    className={cn(
+      "px-4 py-2 rounded-md bg-blue-600 text-white font-semibold hover:bg-blue-700 transition-all",
+      className
+    )}
+    {...props}
+  >
+    {children}
+  </button>
+);
